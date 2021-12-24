b23051b8dd730bb6af780328fed047936aa99c33
Zhidkov Egor <zhidkovegor.alexandrovich@gmail.com>
2021-12-25 00:07:57 +0300

    moved function

--- src/lib.js
+++ src/lib.js
@@ -1,24 +1,3 @@
-import readlineSync from 'readline-sync';
-import getUserName from './cli.js';
+const getRandom = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
-const numberOfRounds = 3;
-const congratulateUser = (userName) => console.log(`Congratulations, ${userName}!`);
-
-export const isVerifiesUserResponse = (rulesGame, gameParameters) => {
-  const userName = getUserName();
-  console.log(`${rulesGame}`);
-  for (let i = 0; i < numberOfRounds; i += 1) {
-    const [questionGame, correctAnswer] = gameParameters();
-    console.log(questionGame);
-    const userAnswer = String(readlineSync.question('Your answer: '));
-    if (correctAnswer !== userAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
-    console.log('Correct!');
-  }
-  congratulateUser(userName);
-};
-
-export const getRandom = (min, max) => Math.floor(Math.random() * (max - min) + min);
+export default getRandom;
