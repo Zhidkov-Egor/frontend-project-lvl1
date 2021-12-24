b8a6ff2542bb11941eae0f2f0d9fd3a993a8693b
Zhidkov Egor <zhidkovegor.alexandrovich@gmail.com>
2021-12-25 00:06:43 +0300

    deleted return

--- src/cli.js
+++ src/cli.js
@@ -1,14 +1,12 @@
 import readlineSync from 'readline-sync';
 
-const getUserName = () => {
-  const gameGreeting = 'Welcome to the Brain Games!';
-  console.log(gameGreeting);
+const runNameGame = () => {
+  console.log('Welcome to the Brain Games!');
   let userName = String(readlineSync.question('May I have your name? '));
   if (userName.trim() === '') {
     userName = 'Unknown';
   }
   console.log(`Hello, ${userName}!`);
-  return userName;
 };
 
-export default getUserName;
+export default runNameGame;
