b27d7b05d4edbd3c538c54b7a4f3fcdfd8f4efe9
Zhidkov Egor <zhidkovegor.alexandrovich@gmail.com>
2021-12-27 19:53:00 +0300

    update function getLargestDivisor

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -3,22 +3,18 @@ import runGame from '../index.js';
 
 const gameQuestion = 'Find the greatest common divisor of given numbers.';
 
-const getLargestDivisor = (first, second) => {
-  const number = (first > second) ? second : first;
-  let result = 1;
-  for (let i = 2; i <= number; i += 1) {
-    if (first % i === 0 && second % i === 0) {
-      result = i;
-    }
+const getLargestDivisor = (first, second, divisor = first) => {
+  if (first % divisor === 0 && second % divisor === 0) {
+    return divisor;
   }
-  return String(result);
+  return getLargestDivisor(first, second, divisor - 1);
 };
 
 const getGameParameters = () => {
   const randomNumberOne = getRandom(1, 100);
   const randomNumberTwo = getRandom(1, 100);
   const questionGame = `${randomNumberOne} ${randomNumberTwo}`;
-  const correctAnswer = getLargestDivisor(randomNumberOne, randomNumberTwo);
+  const correctAnswer = String(getLargestDivisor(randomNumberOne, randomNumberTwo));
   return [questionGame, correctAnswer];
 };
 
