dc36a9ab4e2e309f0a1ceb4bb20d49b1613f5bdb
Zhidkov Egor <zhidkovegor.alexandrovich@gmail.com>
2021-12-27 20:58:25 +0300

    rename function divisor

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -3,13 +3,13 @@ import runGame from '../index.js';
 
 const gameQuestion = 'Find the greatest common divisor of given numbers.';
 
-const getDivisor = (first, second) => (second === 0 ? first : getDivisor(second, first % second));
+const getGcd = (first, second) => (second === 0 ? first : getGcd(second, first % second));
 
 const getGameParameters = () => {
   const randomNumberOne = getRandom(1, 100);
   const randomNumberTwo = getRandom(1, 100);
   const questionGame = `${randomNumberOne} ${randomNumberTwo}`;
-  const correctAnswer = String(getDivisor(randomNumberOne, randomNumberTwo));
+  const correctAnswer = String(getGcd(randomNumberOne, randomNumberTwo));
   return [questionGame, correctAnswer];
 };
 
