c816e0f58a1ec2ff5d21a78ff7f13b6fefe5e7d5
Zhidkov Egor <zhidkovegor.alexandrovich@gmail.com>
2021-12-27 20:44:49 +0300

    upgrade function getLargestDivisor

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -3,11 +3,8 @@ import runGame from '../index.js';
 
 const gameQuestion = 'Find the greatest common divisor of given numbers.';
 
-const getLargestDivisor = (first, second, divisor = first) => {
-  if (first % divisor === 0 && second % divisor === 0) {
-    return divisor;
-  }
-  return getLargestDivisor(first, second, divisor - 1);
+const getLargestDivisor = (first, second) => {
+  return (second === 0) ? first : getLargestDivisor(second, first % second);
 };
 
 const getGameParameters = () => {
